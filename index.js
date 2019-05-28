127c0642bd2fd2da23e7f869b7cc9565e9bcb211
오정훈 <ohchangmin2007@gmail.com>
2019-05-28 14:04:21 -0400

    index.js created

---
+++ index.js
@@ -0,0 +1,8 @@
+<script src="http://code.jquery.com/jquery-3.4.1.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+$('.menu-toggle').click(function(){
+  $('nav').toggleClass('active')
+})
+})
+</script>
